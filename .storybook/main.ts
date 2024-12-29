1b2385c48bbd52d86ef9481fc5fc8af1fb7d39b9
masatotezuka <tez.0731.mst@gmail.com>
2024-12-29 18:21:08 +0900

    change stories path

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,7 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import type { StorybookConfig } from "@storybook/nextjs"
 
 const config: StorybookConfig = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
@@ -16,5 +13,5 @@ const config: StorybookConfig = {
     options: {},
   },
   staticDirs: ["../public"],
-};
-export default config;
+}
+export default config
